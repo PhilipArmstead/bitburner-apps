b9975c258917fb1e9a50adc0d8bf8fc8d201dc28
Phil <philarmstead@mailbox.org>
2022-01-10 22:43:42 +0000

    Changing build rules

--- packages/in-game-browser/vite.config.ts
+++ packages/in-game-browser/vite.config.ts
@@ -1,18 +1,57 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import svgLoader from 'vite-svg-loader'
+
+const bundleFilename = 'main.ts'
+const stylesheetName = 'style.css'
 
 export default defineConfig({
-	// build: {
-	// 	outDir: `../../${WEB_ROOT}`,
-	// },
-	// plugins: [vue(), svgLoader()],
 	plugins: [vue()],
 	build: {
+		lib: {
+			entry: bundleFilename,
+			name: bundleFilename,
+			fileName: () => bundleFilename,
+			formats: ['iife'],
+		},
+		cssCodeSplit: false,
 		rollupOptions: {
-			output: {
-				manualChunks: undefined,
-			},
+			plugins: [
+				{
+					apply: 'build',
+					enforce: 'post',
+					name: 'pack-css',
+					generateBundle(opts, bundle) {
+						const {
+							[stylesheetName]: { source: rawCss },
+							[bundleFilename]: component,
+						} = bundle
+
+						component.code = `
+const winnerdow = globalThis
+const rockument = globalThis['document']
+const bundledCss = ${JSON.stringify(rawCss)}
+
+function mount() {${component.code.replaceAll(/\bwindow\b/ig, 'winnerdow').replaceAll(/\bdocument\b/ig, 'rockument').replaceAll(/\bns\b/ig, 'n_s')}}
+
+export async function main() {
+	const doc = rockument
+	const id = 'theme-browser-app'
+
+	doc.getElementById(id)?.remove()
+	doc.body.insertAdjacentHTML('beforeend', \`<section id='\${id}'></section>\`)
+
+	doc.getElementById(\`\${id}-css\`)?.remove()
+	doc.head.insertAdjacentHTML('beforeend', \`<style id='\${id}-css'>\${bundledCss}</style>\`)
+
+	mount()
+}
+`
+
+						// remove from final bundle
+						delete bundle[stylesheetName]
+					},
+				},
+			],
 		},
 	},
 })
