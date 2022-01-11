fe9dcdb72704ad07613b5d5de984e2abf920f844
Phil <philarmstead@mailbox.org>
2022-01-11 18:25:48 +0000

    Adding semi-colon to wrapper function so closing square bracket isn't joined with IIFE on the following line

--- packages/in-game-browser/src/bundler/wrapper.js
+++ packages/in-game-browser/src/bundler/wrapper.js
@@ -12,7 +12,7 @@ const bundledCss = ${JSON.stringify(css)}
 
 function mount() {
 	const ${windowReplacement} = globalThis
-	const ${documentReplacement} = globalThis['document']
+	const ${documentReplacement} = globalThis['document'];
 	${code.replaceAll(/\bwindow\b/ig, windowReplacement).replaceAll(/\bdocument\b/ig, documentReplacement)}
 }
 `
