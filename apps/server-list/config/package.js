46e30ea4ce1ae0d7888ad2fe0d898ea96037b526
Phil <philarmstead@mailbox.org>
2022-01-20 20:32:41 +0000

    Amending config for auto-upadte

--- apps/server-list/config/package.js
+++ apps/server-list/config/package.js
@@ -2,7 +2,7 @@ import { project } from '@bitburner-theme-browser/common-config'
 
 const { repositoryBranch, repositoryRaw } = project
 
-const packageName = 'macros'
+const packageName = 'server-list'
 export default {
 	appFilePath: `${repositoryRaw}/${repositoryBranch}/apps/${packageName}/dist/main.js`,
 	versionFilePath: `${repositoryRaw}/${repositoryBranch}/apps/${packageName}/dist/version.txt`,
