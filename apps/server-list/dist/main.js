d20d5fad9280da30e9b55f8ec279cd381b440ce9
Phil <philarmstead@mailbox.org>
2022-01-21 00:01:01 +0000

    [server-list] Release: 0.0.41

--- apps/server-list/dist/main.js
+++ apps/server-list/dist/main.js
@@ -7,7 +7,7 @@ export async function main(ns) {
 	// Boilerplate
 	const doc = globalThis['document']
 	const id = 'server-list'
-	globalThis[`${id}-version`] = '0.0.40'
+	globalThis[`${id}-version`] = '0.0.41'
 
 	let vueLoaded
 	const vueLoad = new Promise((resolve) => (vueLoaded = resolve))
@@ -75,7 +75,6 @@ ${Object.entries(ns.ui.getTheme()).map(([key, value]) => `--${key}: ${value};`).
 		await ns.asleep(2000)
 	}
 
-	})
 } 
 
 
